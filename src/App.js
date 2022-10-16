5536b6943a57cc85bf011d0a86acfacb2bb2853a
jaRue1 <jaruejohnson@outlook.com>
2022-10-16 10:28:23 -0400

    setup project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import UploadImageToS3WithNativeSdk from "./components/UploadImageToS3WithNativeSdk"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <UploadImageToS3WithNativeSdk />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
